8bf75c8b56705d2b0f85255093923378af77591f
wissensalt <fauzi.knightmaster.achmad@gmail.com>
2021-07-15 18:05:52 +0700

    add kafka

---
+++ learn-docker/php-simple-app/simple-app/index.php
@@ -0,0 +1,11 @@
+<?php 
+    <html>
+        <h1>Displaying Current PHP Version</h1>
+            echo 'YES PHP IS UP...'
+
+        <div>    
+            echo 'Current PHP version: ' . phpversion();        
+        </div>
+
+    </html>
+?>
